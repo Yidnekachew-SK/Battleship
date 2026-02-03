b4f47b3a0fe81cb1798f9cdaae82da0670e7d7ff
Yidnekachew-SK <yidnekachewsk@gmail.com>
2026-02-03 14:45:52 +0300

    Install packages and write code for ship implementation
    Installed jest and write test codes as well as codes for ship implementation

---
+++ src/ship.js
@@ -0,0 +1,21 @@
+const Ship = function(length) {
+	let hits = 0;
+	let sunk = false;
+
+	const hit = () => {
+		hits++;
+		if (hits >= length) {
+			sunk = true;
+		}
+
+		return hits;
+	};
+
+	const isSunk = () => {
+		return sunk;
+	};
+
+	return { length, hit, isSunk, get hits() { return hits} };
+}
+
+module.exports = Ship;
---
+++ test/ship.test.js
@@ -0,0 +1,22 @@
+const Ship = require('../src/ship');
+
+let ship;
+beforeEach(() => {
+	ship = new Ship(3);
+})
+
+test("ship starts with given length", () => {
+	expect(ship.length).toBe(3);
+});
+
+test("ship takes hits", () => {
+	ship.hit();
+	expect(ship.hits).toBe(1);
+});
+
+test("ship sunk after 3 hits", () => {
+	ship.hit();
+	ship.hit();
+	ship.hit();
+	expect(ship.isSunk()).toBe(true);
+});
